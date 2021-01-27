2d9ef3c974b5a0dfc81e3a15d986ea0d66f09731
mahnerak <mahnerak@gmail.com>
2021-01-27 18:10:46 +0000

    Reformatting

--- arp/poolers.py
+++ arp/poolers.py
@@ -6,11 +6,9 @@ from allennlp.modules.seq2vec_encoders import Seq2VecEncoder
 from allennlp.nn.util import get_final_encoder_states
 
 
-@Seq2VecEncoder.register('at')
+@Seq2VecEncoder.register("at")
 class AtIndexPooler(Seq2VecEncoder):
-    def __init__(self,
-                 embedding_dim: int,
-                 index: int = 1):
+    def __init__(self, embedding_dim: int, index: int = 1):
         super().__init__()
         self._embedding_dim = embedding_dim
         self._index = index
@@ -31,7 +29,11 @@ class AtIndexPooler(Seq2VecEncoder):
             return tokens[:, self._index, :]
         else:  # [CLS] at the end
             if mask is None:
-                raise ValueError("Must provide mask for [MASK] tokens with fixed negative index.")
+                raise ValueError(
+                    "Must provide mask for [MASK] tokens with fixed negative index."
+                )
             if self._index != -1:
-                raise NotImplementedError("Only -1 negative index is supported as of now.")
+                raise NotImplementedError(
+                    "Only -1 negative index is supported as of now."
+                )
             return get_final_encoder_states(tokens, mask)
